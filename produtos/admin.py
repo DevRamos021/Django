4278468812d4e8e34b9749cf0bf1e0eaab7d608c
NIT0311888 <ramos021sg@gmail.com>
2025-08-12 16:37:03 -0300

    Commit de mudanças.

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Categoria,Produto
+from django.utils.html import format_html
 
-admin.site.register(Produto)
+
+@admin.register(Categoria)
+
+class CategoriaAdmin(admin.ModelAdmin):
+
+    list_display = ('nome',)
+
+
+@admin.register(Produto)
+
+class ProdutoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nome',
+        'categoria',
+        'preco',
+        'estoque',
+        'promocao',
+
+    )
+
+    list_filter = ('categoria', 'promocao')
+
+    search_fields = ('nome', 'descricao')
+
+
--- produtos/models.py
+++ produtos/models.py
@@ -1,8 +1,41 @@
-from django.db import models
+from django.utils import timezone
 
-class Produto(models.Model):
+from django.db import models
+from django.template.backends import django
+
+
+
+class Categoria(models.Model):
     nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    descricao = models.TextField(blank=True, null=True)
+
+    estoque = models.IntegerField(default=0)
+
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+
+    promocao = models.BooleanField(default=False)
+
+    data_descricao = models.DateField(default=timezone.now)
+
+    produto = models.CharField(max_length=100)
+
+
+
+
+
+
+
+
+
